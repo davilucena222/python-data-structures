af656e311afda5a1e2fe756c3ce6adc13afa540d
Davi Luiz <davilucena222@gmail.com>
2023-02-01 22:07:36 -0300

    hangman code

---
+++ hangman/index.py
@@ -0,0 +1,38 @@
+import random
+from words import words
+import string
+
+def get_valid_words(words):
+    word = random.choice(words)
+
+    # choose a word without '-' and spaces
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+def hangman():
+    word = get_valid_words(words)
+    word_letters = set(word) # scrambling letters
+    alphabet = set(string.ascii_uppercase) # creating a set of an alphabet
+    used_letters = set() # what the user has guessed or typed
+
+    while len(word_letters) > 0:
+      print("Letters that you already used: ", " ".join(used_letters))
+
+      word_list = [letter if letter in used_letters else "-" for letter in word] # ["a", "b", "c"]
+      print("Current word: ", " ".join(word_list))
+
+      user_letter = input("Guess a letter: ").upper()
+
+
+      if user_letter in alphabet - used_letters: 
+          used_letters.add(user_letter)
+          if user_letter in word_letters:
+              word_letters.remove(user_letter)
+      elif user_letter in used_letters:
+          print("You have already used that character. Please try again.")
+      else:
+          print("You have entered an invalid character. Please try again.")
+    
+hangman()
